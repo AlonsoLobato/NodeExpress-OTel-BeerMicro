afa546e2fff9cdd892f308f8a86d7facca991098
AlonsoLo <alonso.lobato.jimenez@gmail.com>
2024-01-27 00:04:33 -0500

    update

--- tracer.js
+++ tracer.js
@@ -1,17 +1,15 @@
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');                      // Traces
 const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');                     // Span processor
-const { Resource } = require('@opentelemetry/resources');                                     // Traces exporter
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');        // Semantic Conventions - define a common set of (semantic) attributes which provide meaning to data when collecting, producing and consuming it. Specify common names for different kinds of operations and data
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');               // API allows to specify which TracerProvider and/or MeterProvider to use by the given options object.
+const { Resource } = require('@opentelemetry/resources');                                     // Add custom attributes
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');        // Specify common names for different kinds of operations and data
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');             // Traces exporter
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');               // Register instrumentation
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');         // Auto instrum for express module
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');               // Auto instrum for http and https modules
 const { AmqplibInstrumentation } = require('@opentelemetry/instrumentation-amqplib');         // Auto instrum for RabbitMQ module
 const { MongooseInstrumentation } = require('@opentelemetry/instrumentation-mongoose');       // Auto instrum for Mongoose module
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');              // Troubleshooting functionality: enables debug logging.
 
-// Troubleshooting functionality: enables debug logging. 
-// Printing to the console is not recommended for production but only for debugging purposes.
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
 const init = (serviceName) => {
